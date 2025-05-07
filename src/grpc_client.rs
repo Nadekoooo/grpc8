01af19fc2ec4f892dde9841e37c51b0b944bc87c
Nadekoooo <christianyudistira09@gmail.com>
2025-05-07 13:53:47 +0700

    Transaction Service Implementation

--- src/grpc_client.rs
+++ src/grpc_client.rs
@@ -1,5 +1,7 @@
 use services::payment_service_client::PaymentServiceClient;
 use services::PaymentRequest;
+use services::transaction_service_client::TransactionServiceClient;
+use services::TransactionRequest;
 
 
 pub mod services {
@@ -17,6 +19,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let response = client.process_payment(request).await?;
     println!("RESPONSE={:?}", response.into_inner());
+    let mut transaction_client = TransactionServiceClient::connect("http://[::1]:50051").await?;
+    let request = tonic::Request::new(TransactionRequest {
+        user_id: "user_123".to_string(),
+    });
+
+    let mut stream = transaction_client.get_transaction_history(request).await?.into_inner();
+    while let Some(transaction) = stream.message().await? {
+        println!("Transaction: {:?}", transaction);
+    }
 
     Ok(())
 }
